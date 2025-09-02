f632749a5e0e42756940ac356fab3bcdd83fefdb
Christopher Bennell <cbennell@hey.com>
2025-09-02 13:29:37 -0400

    Tidy JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,13 @@
-import "../css/index.css";
+// As per https://vite.dev/guide/backend-integration.html
+import "vite/modulepreload-polyfill";
+
+// Code syntax highlighting
 import "prismjs";
 
+// CSS entrypoint
+import "../css/index.css";
+
+// Vite hot module reload
 if (import.meta.hot) {
   import.meta.hot.accept(() => {
     console.log("HMR: JS updated");
