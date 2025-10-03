a8f36ec0d0e86e307d1562f46c9ce553ef746b2a
Christopher Bennell <cbennell@hey.com>
2025-10-03 18:02:07 -0400

    Add some languages to prism

--- assets/js/prism.js
+++ assets/js/prism.js
@@ -1,8 +1,12 @@
 import "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
+import "prismjs/components/prism-markup-templating";
+import "prismjs/components/prism-erb";
 import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-json";
 import "prismjs/components/prism-ruby";
 import "prismjs/components/prism-markup";
+import "prismjs/components/prism-yaml";
 
 // import "prismjs/plugins/line-numbers/prism-line-numbers";
 // import "prismjs/plugins/line-numbers/prism-line-numbers.css";
